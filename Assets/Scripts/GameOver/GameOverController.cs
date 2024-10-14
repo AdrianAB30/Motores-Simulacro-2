c407b53e069aa40625f4428afc3a4da6af584c2c
Adrian AB <jose.anchiraico@tecsup.edu.pe>
2024-10-13 19:37:37 -0500

    Game Finished with score table

--- Assets/Scripts/GameOver/GameOverController.cs
+++ Assets/Scripts/GameOver/GameOverController.cs
@@ -3,13 +3,45 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
 using UnityEngine.SceneManagement;
+using TMPro;
 public class GameOverController : MonoBehaviour
 {
     public Button btnPlay;
-    // Start is called before the first frame update
+    public TMP_Text scoreText;
+    public TMP_Text bestScoreMessage;
+    public TMP_Text topScoresText;
+    public PlayerSOS playerScore;
+
     void Start()
     {
         btnPlay.onClick.AddListener(() => ReturnMenu());
+        ShowScore();
+        DisplayTopScores();
+        scoreText.gameObject.SetActive(true);
+    }
+    private void ShowScore()
+    {
+        scoreText.text = "Tu Puntaje: " + playerScore.score;
+        bestScoreMessage.gameObject.SetActive(true);
+
+        if (playerScore.IsInTopScores())
+        {
+            bestScoreMessage.text = "Felicitaciones, estas en el top 10";
+        }
+        else
+        {
+            bestScoreMessage.text = "Intentalo nuevamente, no estas en el top 10";
+        }
+    }
+    private void DisplayTopScores()
+    {
+        List<int> topScores = playerScore.GetScores();
+        topScoresText.text = ""; 
+
+        for (int i = 0; i < topScores.Count; i++)
+        {
+            topScoresText.text += "Top " + (i + 1) + ": " + topScores[i] + "\n";
+        }
     }
 
     void ReturnMenu()
--- Assets/Scripts/Scripts Adrian/ScriptableObjects/PlayerSOS.cs
+++ Assets/Scripts/Scripts Adrian/ScriptableObjects/PlayerSOS.cs
@@ -7,8 +7,15 @@ using UnityEngine;
 public class PlayerSOS : ScriptableObject
 {
     public List<int> scores = new List<int>(); 
-    public int score; 
+    public int score;
 
+    private void OnEnable()
+    {
+        if (scores.Count == 0)
+        {
+            scores.AddRange(new int[] { 100, 90, 80, 70, 60, 50, 40, 30, 20, 10 });
+        }
+    }
     public void AddScore(int newScore)
     {
         scores.Add(newScore); 
@@ -21,9 +28,8 @@ public class PlayerSOS : ScriptableObject
     {
         if (score > 0) 
         {
-            scores.Add(score); 
+            score = 0;
         }
-        score = 0; 
     }
     public List<int> GetScores()
     {
@@ -31,8 +37,13 @@ public class PlayerSOS : ScriptableObject
     }
     public bool IsInTopScores()
     {
-        scores.Sort();
-        scores.Reverse();
-        return scores.Contains(score);
+        for (int i = 0; i < scores.Count; i++)
+        {
+            if (score >= scores[i])
+            {
+                return true;
+            }
+        }
+        return false;
     }
 }
